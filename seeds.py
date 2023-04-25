8789cb55335120621e19b61cc4570b44e1a91f15
dylanquaale <dylanquaale33@gmail.com>
2023-04-25 16:34:24 -0500

    created seeds

---
+++ seeds.py
@@ -0,0 +1,23 @@
+from app.models import User
+from app.db import Session, Base, engine
+
+# drop and rebuild tables
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+db = Session()
+
+# insert users
+# use add_all method and User to create new users
+db.add_all([
+  User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+  User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
+  User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+  User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+  User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+
+# to run the insert statements you need to call db.commit()
+db.commit()
+# close the session by using db.close()
+db.close()
