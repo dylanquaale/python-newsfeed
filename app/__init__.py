18e14693b3a05ed494eefbe3f4ac0e883a1fe188
dylanquaale <dylanquaale33@gmail.com>
2023-04-25 14:29:22 -0500

    dashboard works

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,6 @@
-# from flask import Flask
-# from app.routes import home
-
-# def create_app(test_config=None):
-#   # set up app config
-#   app = Flask(__name__, static_url_path='/')
-#   app.url_map.strict_slashes = False
-#   app.config.from_mapping(
-#     SECRET_KEY='super_secret_key'
-#   )
-
-#   return app
-
-# ## this is the decorator
-
-# @app.route('/hello')
-# def hello():
-#   return 'hello world'
-
-# # register routes
-# app.register_blueprint(home)
-
-# return app
-
 from flask import Flask
-from app.routes import home
+from app.routes import home, dashboard
+
 def create_app(test_config=None):
   # set up app config
   app = Flask(__name__, static_url_path='/')
@@ -36,5 +13,6 @@ def create_app(test_config=None):
     return 'hello world'
   # register routes
   app.register_blueprint(home)
-
+  app.register_blueprint(dashboard)
+  
   return app
