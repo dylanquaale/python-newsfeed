d7bf812950a220d4c3c7e6274242cf9c9de1bc47
dylanquaale <dylanquaale33@gmail.com>
2023-04-26 16:21:43 -0500

    working on votes

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,19 +1,26 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
+
 
 class Post(Base):
-  __tablename__ = 'posts'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(100), nullable=False)
-  post_url = Column(String(100), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  # datime module python built in to generate timestamps
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    # datime module python built in to generate timestamps
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    # func.count adds up the votes
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
+    
+    comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
+    user = relationship('User')
 
-  user = relationship('User')
-
-#In MySQL, an ON DELETE CASCADE statement deletes the corresponding foreign key records when a record from the specified table is deleted. In this case, deleting a post from the database also deletes all its associated comments.
-  comments = relationship('Comment', cascade='all,delete')
+# In MySQL, an ON DELETE CASCADE statement deletes the corresponding foreign key records when a record from the specified table is deleted. In this case, deleting a post from the database also deletes all its associated comments.
