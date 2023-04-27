2eb1f6381fa4f78e0a8e8a332d3b33ba20e94825
dylanquaale <dylanquaale33@gmail.com>
2023-04-26 19:12:29 -0500

    error

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,13 +3,27 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
 # connect to database using env variable
-# engine manages connection to db 
+# engine manages connection to db
 # session generates temporary connections for performing create, read, update, and delete(CRUD operations)
-# Base is a class variable helps us map the models to MySQL tables 
-engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
+# Base is a class variable helps us map the models to MySQL tables
+engine = create_engine(getenv('DB_URL'), echo=True,
+                       pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+
+def get_db():
+    if 'db' not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return g.db
+
+
+def init_db():
+    Base.metadata.create_all(engine)
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,19 +1,33 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
-## import function blueprint
+# import function blueprint
 bp = Blueprint('home', __name__, url_prefix='/')
 
-## define function index
+# define function index
+
+
 @bp.route('/')
 def index():
-  return render_template('homepage.html')
+    # get all posts
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
 
-## define function login 
-## @bp.route is a decorator whatever function returns it becomes a response 
+    return render_template(
+        'homepage.html',
+        posts=posts
+    )
+
+# define function login
+# @bp.route is a decorator whatever function returns it becomes a response
+
+
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+    return render_template('login.html')
+
 
 @bp.route('/post/<id>')
 def single(id):
-    return render_template('single-post.html')  
+    return render_template('single-post.html')
