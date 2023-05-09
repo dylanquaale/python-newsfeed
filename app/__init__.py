62208f30edf132736c56de1d15a4c4574834f4b1
dylanquaale <dylanquaale33@gmail.com>
2023-05-09 14:52:03 -0500

    signup

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    app.register_blueprint(api)
 
     init_db()
 
